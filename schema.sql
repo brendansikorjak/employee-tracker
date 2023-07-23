74b410111b367988301e80a5a4eac6fbdb69cf9a
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2023-07-23 18:27:28 -0400

    added departments and roles tables

--- schema.sql
+++ schema.sql
@@ -3,11 +3,27 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+
+CREATE TABLE departments (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL(10, 2),
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
+);
+
 CREATE TABLE employees (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  department VARCHAR(50),
-  role VARCHAR(50),
-  salary INT(10)
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  -- manager_id INT
 );
--- seeds.sql
+++ seeds.sql
@@ -1,3 +1,13 @@
-INSERT INTO employees (first_name, last_name, department, role, salary)
-VALUES ('Joe', 'T', 'tennis', 'bd', '33'),
-('Brendan', 'S', 'tennis', 'sd', '28');
+INSERT INTO departments (name)
+VALUES ('Tennis'),
+('Soccer'),
+('Golf');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('BD', '33', '1'),
+('SD', '28', '1'),
+('Coach', '20', '1');
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ('Joe', 'T', '1'),
+('Brendan', 'S', '2');
